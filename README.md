bbb54816e7afa843cf818f3c1ff0bab9a100e8ed
Evan Boehs <evan@boehs.org>
2024-11-08 23:05:17 -0500

    make it a proper project

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ pnpm run build
 ```
 
 Made pretty quickly with 11ty, CSS, and a bit of JavaScript. Toyed with merging CSS gradients in the header, and beatles in the footer
+
+## Structure
+
+- `_data`: Data files used for site configuration
+  `_includes`: Reusable components
+- `_public`: Files that are static
+  - `_public/admin`: Decap CMS configuration
+- `pages`: Markdown source
+- `scripts`: JavaScript source
+- `styles`: CSS source
--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,59 @@
+import postcss from "postcss";
+import postcsso from "postcss-csso";
+import { build as esbuild } from "esbuild";
+import { minify as minifyHtml } from "html-minifier";
+import { minify as minifyTs } from "terser";
+import * as sass from "sass";
+import path from "path";
+
 export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ _public: "/" });
   eleventyConfig.addFilter("dropContentFolder", (path, folder) =>
     path.replace(new RegExp(folder + "/"), "")
   );
+
+  eleventyConfig.addTemplateFormats("scss");
+  eleventyConfig.addExtension("scss", {
+    outputFileExtension: "css",
+    compile: async function (inputContent, inputPath) {
+      if (inputPath.split("/").at(-1).startsWith("_")) return;
+
+      let { css, loadedUrls } = sass.compileString(inputContent, {
+        loadPaths: [path.parse(inputPath).dir || "."],
+        sourceMap: false,
+      });
+
+      this.addDependencies(inputPath, loadedUrls);
+
+      return async () => {
+        const { content } = await postcss([
+          postcsso({
+            restructure: true,
+          }),
+        ]).process(css, {
+          from: undefined,
+        });
+
+        return content;
+      };
+    },
+    compileOptions: {
+      permalink: function (contents, inputPath) {
+        return inputPath.replace("styles/", "");
+      },
+    },
+  });
+
+  eleventyConfig.addTransform("html", function (content) {
+    if (this.page.outputPath && this.page.outputPath.endsWith(".html")) {
+      return minifyHtml(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+    }
+    return content;
+  });
 
   return {
     dir: {
